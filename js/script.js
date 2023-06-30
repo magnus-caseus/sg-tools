5df45b990552c0c6920124cc7c41d1a73ad508fd
Magnus Caseus <magnus.caseus@protonmail.com>
2023-06-29 22:07:38 -0600

    The chunker is chunking

--- js/script.js
+++ js/script.js
@@ -7,17 +7,73 @@ if (typeof localStorage.getItem('settings') === 'string') {
 }
 var s = { // Settings from localStorage or default
   theme: typeof lss.theme === 'string' ? lss.theme : 'dark',
+  charLimit: typeof lss.charLimit === 'string' ? lss.charLimit : 240
 }
 document.body.className = s.theme;
 
 var decrementerState = {
   input: "",
   output: "",
-  update: function(value) {
+  update: function (value) {
     decrementerState.input = value;
     decrementerState.output = decrementBracketNumbers(value);
   }
 };
+
+var chunkerState = {
+  charLimit: 240,
+  input: "",
+  outputs: [],
+  copied: [],
+  update: function(value) {
+    chunkerState.input = value;
+    if(value.length == 0) {
+      chunkerState.outputs = [""];
+      return;
+    }
+    var idx = 0;
+    var chunkTotal = 1;
+    var chunkCount = 1;
+    var chunkCountPrev = 1;
+    var paginationLength = 4;
+    var chunkLength = chunkerState.charLimit - paginationLength;
+    var arr = [];
+
+    do { // Getting number of chunks, since even the first chunk needs to know
+      console.log('prev: ', chunkCountPrev, ', curr: ', chunkCount);
+      chunkCountPrev = chunkCount;
+      chunkTotal = chunkCount;
+      chunkCount = 0;
+      idx = 0;
+      while(idx < value.length) {
+        idx += chunkLength;
+        chunkCount++;
+        paginationLength = 2 + digitCount(chunkCount) + digitCount(chunkTotal);
+        chunkLength = chunkerState.charLimit - paginationLength;
+      }
+    } while (chunkCount != chunkCountPrev);
+
+    
+    chunkCount = 1;
+    idx = 0;
+    paginationLength = 2 + digitCount(chunkCount) + digitCount(chunkTotal);
+    chunkLength = chunkerState.charLimit - paginationLength;
+    while(idx < value.length) {
+      console.log('chunk: ', chunkCount, ', pagLen: ', paginationLength, ', chunkLen: ', chunkLength);
+      if(idx+chunkLength >= value.length) {
+        arr.push(value.slice(idx).trim() + " " + chunkCount + "/" + chunkTotal);
+      } else {
+        arr.push(value.slice(idx, idx+chunkLength).trim() + " " + chunkCount + "/" + chunkTotal);
+      }
+      chunkCount++;
+      idx += chunkLength;
+      paginationLength = 2 + digitCount(chunkCount) + digitCount(chunkTotal);
+      chunkLength = chunkerState.charLimit - paginationLength;
+    }
+    chunkerState.outputs = arr;
+    chunkerState.copied = Array(arr.length).fill(false);
+  }
+}
 
 
 // Data End
@@ -29,6 +85,10 @@ function setButtonHighlight(buttonName) {
   document.getElementById(buttonName).classList.add("btn_highlight");
 }
 
+function digitCount(n) {
+  return Math.log(n) * Math.LOG10E + 1 | 0;
+}
+
 var re = /\[(\d+)\]/g;
 function decrementBracketNumbers(input) {
   return input.replace(re, decrementBracketNumber);
@@ -38,7 +98,7 @@ function decrementBracketNumber(val) {
   if (parseInt(val.slice(1, -1)) <= 0) {
     return "[XXXX]";
   } else {
-    return "[" + (parseInt(val.slice(1, -1))-1) + "]";
+    return "[" + (parseInt(val.slice(1, -1)) - 1) + "]";
   }
 }
 
@@ -54,38 +114,52 @@ function copyToClipboard(text) {
     }
   );
 }
+
+function scrollToAnchor( anchor ){
+  var is = (el)=>{return el !== undefined && el !== null};
+  //if you pass an undefined anchor it will scroll to the top of the body
+  var targetEl = is(anchor) ? document.getElementById(anchor) : document.body;
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+  var target = is(targetEl) ? targetEl.getBoundingClientRect().top : 0;
+  console.log('target: ', target, 'scrollTop: ', scrollTop);
+  window.scroll({
+      top: target + scrollTop - 200,
+      left: 0,
+      behavior: "smooth"
+  });
+}
 // Functions End
 // Mithril Components Start
 var Main = {
-  view: function() {
+  view: function () {
     return m("main", [
       m(Header),
-      m("div", {id: "content"}, [
-        
+      m("div", { id: "content" }, [
+
       ])
     ])
   }
 };
 
 var Header = {
-  view: function() {
+  view: function () {
     return m("header", [
-      
-        m('h1', 'SocialGalactic Tools'),
-        m('button', {id: 'decrementer_btn', onclick: function() {m.route.set("/decrementer")}}, "Prayer Count Decrementer"),
-        m('button', {id: 'chunker_btn', onclick: function() {m.route.set("/chunker")}}, "Thread Chunker"),
-        m('button', {id: 'image-resizer_btn', onclick: function() {m.route.set("/image-resizer")}}, "Image Resizer")
-      
+
+      m('h1', 'SocialGalactic Tools'),
+      m('button', { id: 'decrementer_btn', onclick: function () { m.route.set("/decrementer") } }, "Prayer Count Decrementer"),
+      m('button', { id: 'chunker_btn', onclick: function () { m.route.set("/chunker") } }, "Thread Chunker"),
+      m('button', { id: 'image-resizer_btn', onclick: function () { m.route.set("/image-resizer") } }, "Image Resizer")
+
     ]);
   }
 };
 
 var themeOptions = {
-  oncreate: function() {
+  oncreate: function () {
     document.getElementById(s.theme).checked = true;
   },
-  view: function() {
-    return m('div', {class: 'settings_option_container'}, [
+  view: function () {
+    return m('div', { class: 'settings_option_container' }, [
       m('h3', 'Theme'),
       m('form', {
         class: 'radios_container',
@@ -97,11 +171,11 @@ var themeOptions = {
           document.body.className = s.theme;
         }
       }, [
-        m('div', {class: 'radio_container'}, [
+        m('div', { class: 'radio_container' }, [
           m('label[for=light]', 'Light'),
           m('input[type=radio][id=light][name=theme][value=light]')
         ]),
-        m('div', {class: 'radio_container'}, [
+        m('div', { class: 'radio_container' }, [
           m('label[for=dark]', 'Dark'),
           m('input[type=radio][id=dark][name=theme][value=dark]')
         ])
@@ -111,61 +185,124 @@ var themeOptions = {
 };
 
 var Decrementer = {
-  oncreate: function() {
+  oncreate: function () {
     setButtonHighlight('decrementer_btn');
   },
-  view: function() {
-    return m("section", {class: "decrementer_section"}, [
+  view: function () {
+    return m("section", { class: "decrementer_section" }, [
       m(inputText),
       m(outputText),
     ]);
-    
+
   }
 };
 
 var inputText = {
-  view: function() {
+  view: function () {
     return m("div", [
       m('h3', 'Input'),
       m("textarea.input[placeholder=Paste text here...]", {
         oninput: function (e) { decrementerState.update(e.target.value) }
       }),
-      m('p', {class: 'input_count'}, decrementerState.input.length + ' Characters')
+      m('p', { class: 'input_count' }, decrementerState.input.length + ' Characters')
     ]);
   }
 };
 
 var outputText = {
-  view: function() {
+  view: function () {
     return m("div", [
       m('h3', 'Output'),
-      m('button', { onclick: function() {copyToClipboard(decrementerState.output)}}, 'Copy to Clipboard'),
+      m('button', { onclick: function () { copyToClipboard(decrementerState.output) } }, 'Copy to Clipboard'),
       m('p', decrementerState.output)
     ]);
   }
 };
 
 var Chunker = {
-  oncreate: function() {
+  oncreate: function () {
     setButtonHighlight('chunker_btn');
   },
-  view: function() {
-    return m("section", {class: "chunker_section"}, [
-      m('h2', 'Under Construction...maybe'),
+  view: function () {
+    return m("section", { class: "chunker_section" }, [
+      m(ChunkerSettings),
+      m(ChunkerInput),
+      m(ChunkerOutput),
     ]);
-    
+
+  }
+};
+
+var ChunkerSettings = {
+  view: function () {
+    return m("form", {onsubmit: function(e) {
+      e.preventDefault();
+      chunkerState.update(chunkerState.input);
+    }}, [
+      m("p", {class: "label_container charlimit"}, [
+        m("label.label[for=charlimit]", "Character Limit"),
+        m("span", {class: "value pitch"}, s.pitch),
+      ]),
+      m("input.input[type=number]", {
+        oninput: function (e) {
+          chunkerState.charLimit = e.target.value;
+          s.charLimit = e.target.value;
+          localStorage.setItem('settings', JSON.stringify(s));
+        },
+        value: chunkerState.charLimit,
+        id: "charlimit"
+    })
+    ]);
+  }
+};
+
+var ChunkerInput = {
+  view: function () {
+    return m("div", [
+      m('h3', 'Input Long Post'),
+      m("textarea.input[placeholder=Paste text here...]", {
+        oninput: function (e) { chunkerState.update(e.target.value) }
+      }, chunkerState.input),
+      m('p', { class: 'input_count' }, chunkerState.input.length + ' Characters')
+    ]);
+  }
+};
+
+var ChunkerOutput = {
+  view: function () {
+    return m("div", [
+      m('h3', 'Output Into Chunks'),
+      chunkerState.outputs.map(function(chunkText, index, arr) {
+        var copiedClass = chunkerState.copied[index] ? ' copied':'';
+        var anchor = "chunk"+index;
+        return m('div', {class: 'chunk_container' + copiedClass}, [
+          m('div', {class: 'flex-row'}, [
+            m('p', {id: anchor, class: 'chunk_label'}, 'Chunk ' + (index+1) + '/' + arr.length),
+            m('button', { onclick: function () {
+              copyToClipboard(chunkText);
+              chunkerState.copied[index] = true;
+              m.route.set("/chunker", {
+                "a": anchor
+              });
+              scrollToAnchor(anchor);
+            } },  'Copy Chunk ' + (index+1) + '/' + arr.length + ' to Clipboard')
+          ]),
+          m('p', chunkText)
+        ]);
+      })
+    ]);
   }
 };
 
 var ImageResizer = {
-  oncreate: function() {
+  oncreate: function () {
     setButtonHighlight('image-resizer_btn');
   },
-  view: function() {
-    return m("section", {class: "image-resizer_section"}, [
+  view: function () {
+    return m("section", { class: "image-resizer_section" }, [
       m('h2', 'Under Construction...maybe'),
     ]);
-    
+
   }
 };
 
